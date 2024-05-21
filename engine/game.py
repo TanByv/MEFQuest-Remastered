bad89d136bc75a7203614f34b9bb8f14bed02b61
ensertugrul <134326931+JaythoS@users.noreply.github.com>
2024-05-21 15:19:16 +0300

    Update game.py

--- engine/game.py
+++ engine/game.py
@@ -6,6 +6,7 @@ from .player import Player
 from .light import LightSource
 from .minigame import MiniGame
 
+
 class Game:
     def __init__(self):
         self.width = 1600
@@ -14,6 +15,7 @@ class Game:
         self.__height = self.height // 2
         self.game_is_running = True
         self.FPS = 60
+        self.result = None
 
         self.window = pygame.display.set_mode((self.width, self.height))
         self.screen = pygame.Surface((self.__width, self.__height))
@@ -31,8 +33,8 @@ class Game:
 
         self.offset = vec2(0, 0)
         self.scroll = vec2
-        self.k = 1 / 5 # camera sway co-efficient (default: 1/20)
-        
+        self.k = 1 / 5  # camera sway co-efficient (default: 1/20)
+
     def update(self):
         if self.current_map.start_game_enabled:
             self.current_map = self.maps[1]
@@ -43,14 +45,11 @@ class Game:
         pygame.display.set_caption(f"{fps_caption} | {player_pos_caption}")
         self.screen.fill((5, 5, 5))
 
-        # rects_in_vision = [rect for rect in self.current_map.map_rects if rect.colliderect(self.window.get_rect())]
         self.light.pos = self.player.pos
-        # self.light.pos = vec2(self.screen.get_size())
 
         self.offset += self.k * (self.player.pos - 0.5 * vec2(self.window.get_rect().center) - self.offset)
-        self.scroll = vec2(int(self.offset.x), int(self.offset.y - 75)) # set camera offset here
+        self.scroll = vec2(int(self.offset.x), int(self.offset.y - 75))  # set camera offset here
 
-        # self.light.show(rects_in_vision, self.scroll)
         self.light.show(self.current_map.nearby_rects, self.scroll)
 
         self.current_map.draw(self.scroll, self.player.pos, self)
@@ -72,12 +71,19 @@ class Game:
         quit()
 
     def start_mini_game(self, player_sprite, csv_filename):
+        # Save current dimensions
+        current_dimensions = self.window.get_size()
+
+        # Start minigame
         self.mini_game = MiniGame(player_sprite, csv_filename)
+
+        # Restore main game dimensions after minigame ends
+        self.window = pygame.display.set_mode(current_dimensions)
 
     def handle_key_events(self):
         key = pygame.key.get_pressed()
 
-       # Reset animation if no movement keys are pressed
+        # Reset animation if no movement keys are pressed
         if not key[pygame.K_a] and not key[pygame.K_d]:
             self.player.vel.x = 0
             self.player.update_animation()
@@ -95,25 +101,32 @@ class Game:
         # Update animation if enough frames have passed
         if self.player.counter > self.player.walk_cooldown:
             self.player.update_animation()
-            
+
         # Handle jump animation
         if key[pygame.K_SPACE] and self.player.standing_on_ground:
             self.player.jump()
 
         if key[pygame.K_ESCAPE] and self.current_map == self.maps[0]:
             self.game_is_running = False
-            
+
         if key[pygame.K_f]:
             for x in self.current_map.map1:
                 enemy_coll = pygame.sprite.collide_rect(self.player, x)
                 if enemy_coll:
                     if x.type == "guard":
-                        self.start_mini_game("guard", "guardian")
+                        self.result = self.start_mini_game("guard", "guardian")
                     elif x.type == "kantin":
-                        self.start_mini_game("kantinci", "kantin")
+                        self.result = self.start_mini_game("kantinci", "kantin")
+                    elif x.type == "ilker":
+                        self.result = self.start_mini_game("ilker", "ilkay")
                     elif x.type == "door0":
                         self.player.rect.topleft = (20, 965)
                     elif x.type == "door1":
                         self.player.rect.topleft = (-178, 2210)
                     elif x.type == "door2":
                         self.player.rect.topleft = (-58, 3045)
+
+                if self.result:
+                    pass
+                else:
+                    pass
