5271373e67a37c0e558345174250635d5c38c2e7
Tan <t2adbyv@gmail.com>
2024-05-31 15:32:48 +0300

    Restore original speed

--- engine/game.py
+++ engine/game.py
@@ -15,7 +15,7 @@ class Game:
         self.__width = self.width // 2
         self.__height = self.height // 2
         self.game_is_running = True
-        self.FPS = 160
+        self.FPS = 60
 
         self.window = pygame.display.set_mode((self.width, self.height))
         self.screen = pygame.Surface((self.__width, self.__height))
